3edc9ffd802a4500740373f60ffd85de0a3b3a16
Mukwende <mukwende2000@gmail.com>
2023-06-07 16:12:45 +0200

    complete crew page design all viewports

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 function PageHeading({ title, number }: Props) {
   return (
-    <h1 className="text-[20px] md:text-32 my-10 tracking-widest uppercase">
+    <h1 className="text-[20px] text-center md:text-left md:text-32 my-10 tracking-widest uppercase">
       <span className="text-primary/30 mr-2">{number}</span>
       <span className="text-primary">{title}</span>
     </h1>
--- src/routes/Crew/index.tsx
+++ src/routes/Crew/index.tsx
@@ -3,32 +3,32 @@ import mark from '../../assets/crew/image-mark-shuttleworth.png'
 
 function Crew() {
   return (
-    <main>
+    <main className="min-h-screen">
       <div className="container">
         <PageHeading number="02" title="meet your crew" />
-        <div className="flex flex-col-reverse">
-          <div className="text-center flex flex-col-reverse">
+        <div className="flex flex-col-reverse md:flex-col lg:grid grid-cols-2">
+          <div className="text-center lg:text-left flex flex-col-reverse lg:flex-col py-10">
             <div>
-              <h2 className="text-16 text-secondary uppercase font-bellefair">
+              <h2 className="text-16 lg:text-primary/30 md:text-[24px] lg:text-32 text-secondary uppercase font-bellefair">
                 mission specialist
               </h2>
-              <h3 className="text-[24px] text-primary font-bellefair uppercase mb-3">
+              <h3 className="text-[24px] md:text-[40px] lg:text-56 text-primary font-bellefair uppercase mb-3">
                 mark shuttleworth
               </h3>
-              <p className="text-16">
+              <p className="text-16 lg:text-[18px] md:w-[50%] md:m-auto lg:m-0 lg:w-[444px]">
                 Mark Richard is the founder and ceo of canonical, the company
                 behind the Linux based operating system. Shuttleworth became the
                 first south african to travel to Space as a space tourist.
               </p>
             </div>
-            <div className="flex gap-3 justify-center my-8">
+            <div className="flex gap-3 justify-center lg:justify-start mb-5 lg:mt-10">
               <div className="w-[10px] h-[10px] bg-white rounded-full"></div>
               <div className="w-[10px] h-[10px] bg-white rounded-full"></div>
               <div className="w-[10px] h-[10px] bg-white rounded-full"></div>
               <div className="w-[10px] h-[10px] bg-white rounded-full"></div>
             </div>
           </div>
-          <div className="w-[153px] m-auto border-b border-gray-500">
+          <div className="w-[153px] md:w-[368px] lg:w-[443px] lg:-mt-28 lg:mr-80 m-auto border-b border-gray-500">
             <img src={mark} alt="Mark shuttleworth" className="w-full" />
           </div>
         </div>
